de533f8ac8cd070e97dbaa1e482547244583cc60
ShinySilver <nicolas.louismarie@gmail.com>
2020-11-06 15:25:40 +0100

    Added database serialization

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,6 +1,9 @@
 import random
 import time
+import datetime
 import numpy as np
+import resources
+import os
 
 from qibullet import SimulationManager
 from qibullet import PepperVirtual
@@ -26,19 +29,36 @@ class Engine:
         img = self.__agent.getCameraFrame(self._camera_handle)
         sensors = self.__agent.getFrontLaserValue()
         cube.remove()
-        return {'X':(img, sensors),'Y':(cubeRGB, cubeSize)}
+        return (img, sensors, cubeRGB, cubeSize)
+
+    def genDataset(self, output_path, size, nthreads=1, seed=None):
+        r = random.Random(seed)
+        img=[]
+        sensors=[]
+        cubeRGB=[]
+        cubeSize=[]
+        for i in range(size):
+            line = engine.genClassData(r.random() + 0.5,  # CubeRho
+                                      r.random() * np.pi / 3 - np.pi / 6,  # CubeTeta
+                                      r.random() * 2 * np.pi,  # CubeRotZ
+                                      (r.random(), r.random(), r.random()),  # RGB
+                                      r.random() * 1.5 + 0.5)
+            img+=[line[0]]
+            sensors+=[line[1]]
+            cubeRGB+=[line[2]]
+            cubeSize+=[line[3]]
+        os.makedirs(os.path.dirname(output_path), exist_ok=True)
+        np.savez(output_path, img=img, sensors=sensors, cubeRGB=cubeRGB, cubeSize=cubeSize)
 
 if __name__ == "__main__":
     engine = Engine()
-    r = random.Random()
 
-    for i in range(10):
-        img = engine.genClassData(r.random()+0.5,           # CubeRho
-                            r.random()*np.pi/3-np.pi/6,         # CubeTeta
-                            r.random()*2*np.pi,                 # CubeRotZ
-                            (r.random(),r.random(),r.random()), # RGB
-                            r.random()*1.5+0.5)['X'][0]
-        plt.figure()
-        plt.imshow(img, interpolation='nearest')
-    plt.show()
+    size = 10
+    dataset_path = os.path.join(resources.DATASETS, f"dataset.npz")
+    if not os.path.exists(dataset_path):
+        engine.genDataset(dataset_path, size)
+        print(f'generated and saved dataset at "{dataset_path}"')
+
+    dataset=np.load(dataset_path)
+    print(f'loaded previously generated dataset at "{dataset_path}"')
 
---
+++ resources/__init__.py
@@ -0,0 +1,4 @@
+import os
+
+PATH = os.path.dirname(__file__)
+DATASETS = os.path.join(PATH, 'datasets')
